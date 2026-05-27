b56bc3d1814d7e0c92834efe48444a0ea47f1a44
mrafiajk@gmail.com <mrafiajk@gmail.com@>
2026-05-27 11:02:23 +0530

    Add tutor layout

--- Server.js
+++ Server.js
@@ -358,6 +358,11 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+
+app.options("*", cors(corsOptions));
+
+
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
